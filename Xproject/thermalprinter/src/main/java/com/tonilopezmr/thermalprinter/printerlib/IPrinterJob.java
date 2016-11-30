f03f13b2c0a32abca9200d9e533135a68485afe2
Adriano Vega <adrianovega@hotmail.com>
2016-11-30 12:26:22 +0100

    Added new methods to PrinterJobImpl and PrinterBluetooth

--- Xproject/thermalprinter/src/main/java/com/tonilopezmr/thermalprinter/printerlib/IPrinterJob.java
+++ Xproject/thermalprinter/src/main/java/com/tonilopezmr/thermalprinter/printerlib/IPrinterJob.java
@@ -3,20 +3,29 @@ package com.tonilopezmr.thermalprinter.printerlib;
 public interface IPrinterJob {
 
 
-    /**
-     * Method that initializes the printer to start a Job Session
-     */
-    public void initializePrinter();
-    /**
-     * Method that prints the arg line using the settings used in the printer
-     * @param line
-     */
-    public void printLine(String line);
+  /**
+   * Method that initializes the printer to start a Job Session
+   */
+  public void initializePrinter() throws PrinterJobException;
+  /**
+   * Method that prints the arg line using the settings used in the printer
+   * @param line
+   */
+  public void printLine(String line) throws PrinterJobException;
 
-    /**
-     * Method that prints a line with underscores.
-     * Example: ----------------
-     */
-    public void printUnderLine();
+  /**
+   * Method that prints a separator.
+   * Default: ----------------
+   */
 
+  public void printSeparator() throws PrinterJobException;
+
+  public void setSeparator(String separator);
+
+
+  /** Method that sets the number of newLines that goes after a separator
+   * Default: 2 new lines
+   * @param spacing
+   */
+  public void setSeparatorSpacing(int spacing);
 }
