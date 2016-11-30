f246b3e1c11e2d3ad43f5c3872e1e2c832d20c31
Adriano Vega <adrianovega@hotmail.com>
2016-11-30 13:07:37 +0100

    Changed the commands from string to byte array

--- Xproject/thermalprinter/src/main/java/com/tonilopezmr/thermalprinter/printerlib/PrinterCommands.java
+++ Xproject/thermalprinter/src/main/java/com/tonilopezmr/thermalprinter/printerlib/PrinterCommands.java
@@ -1,6 +1,14 @@
 package com.tonilopezmr.thermalprinter.printerlib;
 
 public class PrinterCommands {
-  public static final String ESC = "ESC";
-  public static final String INITIALIZE = ESC + " @";
+  public static final byte ESC = 0x1B;
+  public static final byte[] INITIALIZE = {ESC, '@'};
+
+  public static final byte ALIGN = (byte)'a';
+
+  public static final byte[] ALIGNMENT_LEFT = {ESC, ALIGN, 0x00};
+  public static final byte[] ALIGNMENT_CENTER = {ESC, ALIGN, 0x01};
+  public static final byte[] ALIGNMENT_RIGHT = {ESC, ALIGN, 0x02};
+
+  
 }
