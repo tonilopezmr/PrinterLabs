fd86fa6b6ca946677068d1ad931a44bcceb38cd9
Adriano Vega <adrianovega@hotmail.com>
2016-12-03 01:05:24 +0100

    Added PaperFeed methods

--- Xproject/thermalprinter/src/main/java/com/tonilopezmr/thermalprinter/printerlib/IPrinter.java
+++ Xproject/thermalprinter/src/main/java/com/tonilopezmr/thermalprinter/printerlib/IPrinter.java
@@ -3,6 +3,8 @@ package com.tonilopezmr.thermalprinter.printerlib;
 import android.content.Context;
 import android.os.Handler;
 
+import com.tonilopezmr.bluetoothprinter.commands.PrinterCommand;
+
 public interface IPrinter {
   public void connect(Context context, Handler messageHandler) throws PrinterException;
   public void disconnect();
@@ -10,6 +12,6 @@ public interface IPrinter {
   public void write(byte[] data) throws PrinterException;
   public void setAlignment(PrinterCommands.Align alignment) throws PrinterException;
   public void setFont(PrinterCommands.Font font) throws PrinterException;
-
+  public void feedPaper(PrinterCommands.FeedPaper feed) throws PrinterException;
   void initialize() throws PrinterException;
 }
